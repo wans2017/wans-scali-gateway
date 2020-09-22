652870cda7533dd72cb7928f02d90933d0305ce4
wans <17340310@qq.com>
2020-09-22 08:32:18 +0800

    Initial commit

---
+++ src/main/java/com/wans/AppGateWay.java
@@ -0,0 +1,14 @@
+package com.wans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by admin on 2020/9/17.
+ */
+@SpringBootApplication
+public class AppGateWay {
+    public static void main(String[] args) {
+        SpringApplication.run(AppGateWay.class);
+    }
+}
---
+++ src/main/java/com/wans/filter/TokenGlobalFilter.java
@@ -0,0 +1,45 @@
+package com.wans.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by admin on 2020/9/18.
+ */
+/*
+GlobalFilter全局过滤器接口，实现filter方法进行校验
+Ordered优先级顺序，值越大则优先级越低
+ */
+@Component
+public class TokenGlobalFilter implements GlobalFilter, Ordered {
+    /*
+    ServerWebExchange服务网络交换器，有点像Context上下文
+    GatewayFilterChain网关过滤链表
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange serverWebExchange, GatewayFilterChain gatewayFilterChain) {
+        // 获取token参数
+        String token = serverWebExchange.getRequest().getQueryParams().getFirst("token");
+        if (StringUtils.isEmpty(token)) {   // token不能为空
+            ServerHttpResponse response = serverWebExchange.getResponse();  // 获取返回
+            response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);       // 设置状态码
+            String msg = "token not is null";
+            DataBuffer buffer = response.bufferFactory().wrap(msg.getBytes()); // 数据缓冲器写入信息
+            return response.writeWith(Mono.just(buffer));
+        }
+        return gatewayFilterChain.filter(serverWebExchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
