32d87de0f8a603714ec6f39a7314a936d18d0826
wans <17340310@qq.com>
2020-09-22 17:22:53 +0800

    xiugai

--- src/main/java/com/wans/filter/TokenGlobalFilter.java
+++ src/main/java/com/wans/filter/TokenGlobalFilter.java
@@ -1,11 +1,13 @@
 package com.wans.filter;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -20,6 +22,8 @@ Ordered优先级顺序，值越大则优先级越低
  */
 @Component
 public class TokenGlobalFilter implements GlobalFilter, Ordered {
+    @Value("${server.port}")
+    private String serverPort;
     /*
     ServerWebExchange服务网络交换器，有点像Context上下文
     GatewayFilterChain网关过滤链表
@@ -35,7 +39,10 @@ public class TokenGlobalFilter implements GlobalFilter, Ordered {
             DataBuffer buffer = response.bufferFactory().wrap(msg.getBytes()); // 数据缓冲器写入信息
             return response.writeWith(Mono.just(buffer));
         }
-        return gatewayFilterChain.filter(serverWebExchange);
+        // 在请求头中存放网关端口号serverPort
+        ServerHttpRequest request = serverWebExchange.getRequest().mutate().header("serverPort", serverPort).build();
+        return gatewayFilterChain.filter(serverWebExchange.mutate().request(request).build());
+        // return gatewayFilterChain.filter(serverWebExchange);
     }
 
     @Override
